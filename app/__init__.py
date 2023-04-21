d55eab9fd839449e23c7d14572e297f9aa6b84e1
davip8a3a@gmail.com <davip8a3a@gmail.com>
2023-04-21 20:51:06 -0300

    Add flask-migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-#teste
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+#rodar a página home 
 app = Flask(__name__)
 @app.route("/")
 def index():
