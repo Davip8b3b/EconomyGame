713ad6880e5af8cc53a4cfe165f8ec98ba9e21d6
davip8a3a@gmail.com <davip8a3a@gmail.com>
2023-04-23 21:16:48 -0300

    Add da migração

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask.cli import FlaskGroup, with_appcontext
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+cli = FlaskGroup(app)
+
+@with_appcontext
+@cli.command()
+def create_db(): 
+  db.create_db()
+  print("Banco de dados criado com sucesso!")
   
 from app.controllers import default
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from app import app
+from app import cli
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  cli()
